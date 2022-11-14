520887c32e9509c102e1a1b382ebdb7da3544272
kaushal <kaushal31rajput@gmail.com>
2022-11-14 20:47:23 +0530

    check

---
+++ vm.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.43.0"
+    }
+  }
+}
+
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+}
